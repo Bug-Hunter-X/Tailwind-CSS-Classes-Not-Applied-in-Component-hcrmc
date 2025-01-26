cc0e2099e15c2b8af7933f32a8bdfd5c35e512c9
BugHunter-X <askzenai@gmail.com>
2025-01-26 14:12:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,9 @@
+```javascript
+//In your MyComponent.js file, import the Tailwind directives:
+import './index.css'; //Import your tailwind styles file 
+
+//Now you can use the classes:
+<div class="bg-red-500 p-4">
+  <h1>This is my component</h1>
+</div>
+```
